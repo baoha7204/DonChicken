5726450e22746ab875ebaa1f5f5879da81cf6198
baoha7204 <baoha3604@gmail.com>
2023-09-18 21:19:04 +0700

    fix scrolling header and update new offer

--- assets/script.js
+++ assets/script.js
@@ -43,11 +43,74 @@ const offers_khoaiTayChien = [
         offer_price: '79.000'
     }
 ];
+
+const newOffers = [
+    {
+        id: 1,
+        offer_name: 'Gà nướng (L)',
+        offer_image: './assets/img/new_offers/1.jpg',
+        offer_price: '269.000'
+    },
+    {
+        id: 2,
+        offer_name: 'Gà rán không xương (L)',
+        offer_image: './assets/img/new_offers/2.jpg',
+        offer_price: '319.000'
+    },
+    {
+        id: 3,
+        offer_name: 'Gà rán sốt tương (L)',
+        offer_image: './assets/img/new_offers/3.jpg',
+        offer_price: '319.000'
+    },
+    {
+        id: 4,
+        offer_name: 'Gà rán sốt cay (L)',
+        offer_image: './assets/img/new_offers/4.jpg',
+        offer_price: '319.000'
+    },
+    {
+        id: 5,
+        offer_name: 'Gà rán (L)',
+        offer_image: './assets/img/new_offers/5.jpg',
+        offer_price: '309.000'
+    },
+    {
+        id: 6,
+        offer_name: 'Gà rán Sốt Ngọt (L)',
+        offer_image: './assets/img/new_offers/6.jpg',
+        offer_price: '319.000'
+    },
+    {
+        id: 7,
+        offer_name: 'Gà Phô Mai Tuyết (L)',
+        offer_image: './assets/img/new_offers/7.jpg',
+        offer_price: '319.000'
+    },
+    {
+        id: 8,
+        offer_name: 'Gà Hallamayo không xương (L)',
+        offer_image: './assets/img/new_offers/8.jpg',
+        offer_price: '340.000'
+    },
+    {
+        id: 9,
+        offer_name: 'Gà Hallamayo có xương (L)',
+        offer_image: './assets/img/new_offers/9.jpg',
+        offer_price: '330.000'
+    },
+    {
+        id: 10,
+        offer_name: 'Gà nướng Teriyaki (L)',
+        offer_image: './assets/img/new_offers/10.jpg',
+        offer_price: '299.000'
+    }
+];
 /**
  * Insert items into the offer list
  */
 function insertOfferItems(array, section) {
-    const offerItemsHTML = array.map((item) =>{
+    let offerItemsHTML = array.map((item) =>{
         return `<div class="offer-item">
         <img class="offer-item__image" src="${item.offer_image}" width="270" height="270" alt="${item.offer_name})"></img>
         <div class="offer-item__detail">
@@ -59,29 +122,54 @@ function insertOfferItems(array, section) {
         </div>
     </div>`;
     });
-    const offerList = $(section);
+    let offerList = $(section + ' .offer-items');
     offerList.innerHTML = offerItemsHTML.join('\n');
+    addAutoSlide(section);
+}
+
+/**
+ * Add buttons into the offer container
+ */
+function addButtonToOfferContainer() {
+    // add buttons
+    let prev = document.createElement('button');
+    let prevNode = document.createTextNode("⬅");
+    prev.appendChild(prevNode);
+    prev.setAttribute('id', 'prev');
+
+    let next = document.createElement('button');
+    let nextNode = document.createTextNode("➡");
+    next.appendChild(nextNode);
+    next.setAttribute('id', 'next');
+
+    var buttons = document.createElement('div');
+    buttons.classList.add('buttons');
+    buttons.append(prev);
+    buttons.append(next);
+    const offer_containers = $$('.offers__container');
+    offer_containers.forEach((offer_container) => {
+        offer_container.append(buttons);
+    });
 }
 
 function switchOfferItems(){
     const catergories = $$('li.offer-catergory');
-    const section = '.offers .offer-items';
+    const section = '.offers';
     insertOfferItems(offers_gaNuong, section);
-    catergories.forEach((catergory) =>{
+    catergories.forEach((catergory, index) =>{
         catergory.addEventListener('click', () =>{
-            catergories.forEach((subCatergory) =>{
-                subCatergory.classList.remove('active');
-            })
-            catergory.classList.add('active');
-            if(catergory.classList.contains('category--gaNuong')){
-                insertOfferItems(offers_gaNuong, section);
-            } else if(catergory.classList.contains('category--khoaiTayChien')){
-                insertOfferItems(offers_khoaiTayChien, section);
+            if(!catergory.classList.contains('active')){
+                let otherIndex = (index == 0) ? 1: 0;
+                catergories[otherIndex].classList.remove('active');
+                catergory.classList.add('active');
+                if(catergory.classList.contains('category--gaNuong')){
+                    insertOfferItems(offers_gaNuong, section);
+                } else if(catergory.classList.contains('category--khoaiTayChien')){
+                    insertOfferItems(offers_khoaiTayChien, section);
+                }
             }
         });
     });
-    // Add automatic slide horizontally
-    addAutoSlide('.offers');
 }
 /**
  * Insert fontAwesome
@@ -142,9 +230,14 @@ function addActiveDropdownMobileItems() {
 function scrollingHeader() {
     document.onscroll = () => {
         const header = $('header');
+        let logo = 0;
+        if (window.matchMedia("(min-width: 320px) and (max-width: 568px)").matches || window.matchMedia("(min-width: 992px) and (max-width: 1268px)").matches) {
+            let header_logo = $('.header-container__logo');
+            logo = header_logo.offsetHeight;
+        }
         var scrollTop = document.documentElement.scrollTop || document.body.scrollTop;
-        console.log(scrollTop);
-        if(scrollTop > 40){
+        var dist = scrollTop - logo;
+        if(dist > 40){
             header.classList.add('header--scrolling');
         } else{
             header.classList.remove('header--scrolling');
@@ -169,26 +262,30 @@ function addActiveScrollbar() {
 /**
  * Add automatic slide horizontally to offers
  */
+var activeOffers = 0;
 function addAutoSlide(section){
-    let slider = $(section + ' .offers-container__items .offer-items');
-    let items = $$(section + ' .offers-container__items .offer-items .offer-item');
-    let next = $(section + ' #next');
-    let prev = $(section + ' #prev');
-    let lengthItems = items.length - 1;
-    let active = 0;
-    next.onclick = function(){
-        active = active + 1 <= lengthItems ? active + 1 : 0;
-        reloadSlider();
-    }
-    prev.onclick = function(){
-        active = active - 1 >= 0 ? active - 1 : lengthItems;
-        reloadSlider();
-    }
-    let refreshInterval = setInterval(()=> {next.click()}, 5000);
-    function reloadSlider(){
-        slider.style.left = -items[active].offsetLeft + 'px';
-        clearInterval(refreshInterval);
-        refreshInterval = setInterval(()=> {next.click()}, 5000);
+    if(window.matchMedia("(max-width: 768px)").matches){
+        let slider = $(section + ' .offers-container__items .offer-items');
+        let items = $$(section + ' .offers-container__items .offer-items .offer-item');
+        var nextBtn = $(section + ' #next');
+        var prevBtn = $(section + ' #prev');
+        let lengthItems = items.length - 1;
+        activeOffers = 0;
+        slider.style.left = 0;
+        nextBtn.onclick = function(){
+            activeOffers = activeOffers + 1 <= lengthItems ? activeOffers + 1 : 0;
+            reloadSlider();
+        }
+        prevBtn.onclick = function(){
+            activeOffers = activeOffers - 1 >= 0 ? activeOffers - 1 : lengthItems;
+            reloadSlider();
+        }
+        let refreshInterval = setInterval(()=> {nextBtn.click()}, 5000);
+        function reloadSlider(){
+            slider.style.left = -items[activeOffers].offsetLeft + 'px';
+            clearInterval(refreshInterval);
+            refreshInterval = setInterval(()=> {nextBtn.click()}, 5000);
+        }
     }
 }
 
@@ -208,7 +305,9 @@ function myWebApp() {
     scrollingHeader();
     // Add click event for scrollbar
     addActiveScrollbar();
+    addButtonToOfferContainer();
     switchOfferItems();
+    insertOfferItems(newOffers, '.new-offers');
 }
 
 myWebApp();
